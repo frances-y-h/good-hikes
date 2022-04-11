2d2765547363b0f7421250540038bc420bb473cb
Frances Huang <frances.y.huang@gmail.com>
2022-04-11 14:37:19 -0700

    setting up branch

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+router.get('/signup',)
+
 
 module.exports = router;
