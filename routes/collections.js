4aa6a350233771fa0cc7fca29c2c68c91ce61e71
Brian May <brianmay2014@gmail.com>
2022-04-12 11:11:19 -0700

    update for merge resolution

--- routes/collections.js
+++ routes/collections.js
@@ -40,20 +40,5 @@ router.get('/:collectionId(\\d+)', asyncHandler( async (req, res) => {
 }));
 
 
-// Get 1 specific hikes
-// router.get(
-// 	"/:hikeId(\\d+)",
-// 	asyncHandler(async (req, res) => {
-// 		const hikeId = parseInt(req.params.hikeId, 10);
-// 		const hike = await db.Hike.findByPk(hikeId, { include: db.Tag });
-// 		const reviews = await db.Review.findAll({ where: { hikeId } });
-
-// 		res.render("hike", {
-// 			title: hike.name,
-// 			hike,
-// 			reviews,
-// 		});
-// 	})
-// );
 
 module.exports = router;
