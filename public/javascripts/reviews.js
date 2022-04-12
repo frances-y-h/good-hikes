ed11be65a0c7dff0b24f52056cf4b044fb207d3c
Lana_K <lana1990.komar@gmail.com>
2022-04-12 11:51:29 -0700

    eventListeners added to Cancel and Submit buttons, styles to the form and buttons added

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,10 +1,59 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     console.log("hi from reviews.js")
 
-    const reviewButton = document.querySelector('#review-btn')
+
+
+    const reviewForm = document.getElementById("review-form");
+    const reviewButton = document.querySelector('.review-btn');
+    const cancelReviewButton = document.querySelector('#review-cancel');
+    const submitReviewButton = document.querySelector('#review-submit');
 
     reviewButton.addEventListener("click", (event) => {
-        const reviewForm = document.getElementById("review-form");
         reviewForm.classList.remove("hidden");
+        hikeId = event.target.id.split("-")[2];
+
+        cancelReviewButton.addEventListener("click", (event) => {
+            reviewForm.classList.add("hidden");
+            ;
+        });
+
+        submitReviewButton.addEventListener("click", async (event) => {
+            event.preventDefault();
+            const rating = document.querySelector('input[name=rating]').value;
+            const comment = document.querySelector('textarea[name=comment]').value;
+            const dateHike = document.querySelector('input[name=dateHike]').value;
+            // console.log(dateHike) 2022-03-31
+
+            const res = await fetch(`/hikes/${hikeId}/reviews`, {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    rating,
+                    comment,
+                    dateHike
+                })
+            })
+
+            const data = await res.json();
+            if (data.message === 'Success') {
+                const review = document.getElementById('review');
+                const reviewId = document.getElementById('review-id'); // change to name
+                const reviewRating = document.getElementById('review-rating');
+                const reviewComment = document.getElementById('review-comment');
+                const reviewDateHike = document.getElementById('review-dateHike');
+
+                reviewId.innerHTML = data.review.id; // change to name
+                reviewRating.innerHTML = data.review.rating;
+
+                if (data.review.comment) {
+                    reviewComment.innerHTML = data.review.comment;
+                }
+                if (data.review.dateHike) {
+                    reviewDateHike.innerHTML = data.review.dateHike;
+                }
+            } else {
+                //errors
+            }
+        });
     });
 });
