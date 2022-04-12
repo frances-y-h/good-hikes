67b7fe8beca8c12cbf34039682875e8771428b5e
Elan Katz <otter2388@gmail.com>
2022-04-11 20:19:10 -0700

    moved seed file generator to db folder and updated references, also deleted demo.js  as no longer needed

--- db/seeders/20220411165904-review-data.js
+++ db/seeders/20220411165904-review-data.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 // import array of object records
-const { reviewSeed } = require('../../public/javascripts/tables-seed-data');
+const { reviewSeed } = require("../tables-seed-data");
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Reviews', reviewSeed, {});
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert("Reviews", reviewSeed, {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Reviews', null, {});
-  }
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete("Reviews", null, {});
+    },
 };
