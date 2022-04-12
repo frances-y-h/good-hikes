85ab910d9f5bcbf7937805d59d305d58a5adebd0
Frances Huang <frances.y.huang@gmail.com>
2022-04-12 09:11:19 -0700

    setting up hike page and some basics for reviews

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const { sessionSecret } = require('./config');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const hikesRouter = require('./routes/hikes');
 
 const app = express();
 
@@ -39,6 +40,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hikes', hikesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
