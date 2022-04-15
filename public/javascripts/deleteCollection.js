075c713b1814fa7ba9edb854c6bcd1e50d644874
Brian May <brianmay2014@gmail.com>
2022-04-15 10:39:19 -0700

    delete modal works

--- public/javascripts/deleteCollection.js
+++ public/javascripts/deleteCollection.js
@@ -12,19 +12,60 @@ document.addEventListener("DOMContentLoaded", async (e) => {
             const collectionId = deleteEvent.target.id.split('-')[0];
             const collectionHikeCount = parseInt(deleteEvent.target.id.split("-")[5], 10);
 
-            //call to the back end
-            const res = await fetch(`/collections/${collectionId}`, {
-                    method: 'DELETE'
-                }
-                );
-            const data = await res.json();
+            console.log(collectionId);
 
-            //if response was successful
-            if (data.message === "Success") {
-                //select the row to hide from view
-                const collectionRow = document.querySelector(`.row-${collectionId}`);
-                collectionRow.remove();
-            } 
+            //grab modal div for each delete button
+            const deleteConfirmDiv = document.getElementById(
+				`delete-confirm-page-${collectionId}`
+			);
+
+
+            const deleteConfirmModal = document.querySelector('.delete-modal');
+
+            //grab the submit and cancel buttons
+            const deleteSubmitButton = document.getElementById(
+                `delete-collection-submit-${collectionId}`
+                );
+            const deleteCancelButton = document.getElementById(
+                `delete-collection-cancel-${collectionId}`
+                );
+
+
+            
+            //show modal
+            deleteConfirmDiv.classList.remove("hide-delete");
+
+            deleteSubmitButton.addEventListener("click", async (submitEvent) => {
+                //call to the back end
+                const res = await fetch(`/collections/${collectionId}`, {
+                        method: 'DELETE'
+                    }
+                    );
+                const data = await res.json();
+    
+                //if response was successful
+                if (data.message === "Success") {
+                    //select the row to hide from view
+                    const collectionRow = document.querySelector(`.row-${collectionId}`);
+                    collectionRow.remove();
+                } 
+
+                deleteConfirmDiv.classList.add("hide-delete");
+
+            });
+
+            deleteCancelButton.addEventListener("click", (cancelEvent) => {
+                deleteConfirmDiv.classList.add("hide-delete");
+            });
+
+            window.onclick = function (event) {
+                if (event.target == deleteConfirmDiv) {
+                    deleteConfirmDiv.classList.add("hide-delete");
+                }
+
+            }
+
+
 
         });
     }
