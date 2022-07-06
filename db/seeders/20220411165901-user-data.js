876274e6a0a3e98d8bedeb680cb5dae6ce538ac0
Elan Katz <otter2388@gmail.com>
2022-07-05 20:44:08 -0700

    update seed data

--- db/seeders/20220411165901-user-data.js
+++ db/seeders/20220411165901-user-data.js
@@ -5,6 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert(
       'Users',
       [
+        //USERID 1
         {
           //   username: 'IfIJustLayHere',
           username: 'DemoUser',
@@ -14,13 +15,53 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
-        //Added USER 2
+        //USERID 2
         {
           //   username: 'SnowPatrolRules',
           username: 'Ansel Adams',
           email: 'snowpatrolrules@alec.com',
           hashedPassword:
             '$2a$10$g92V3Gdj0wAFwMGV.7aS4O8IA.x19y7SaluTGzgL.KnF5e0a31ZKC',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //USERID 3
+        {
+          //   password: 'password',
+          username: 'Frances',
+          email: 'frances@user.io',
+          hashedPassword:
+            '$2a$10$NMkHOnZRabJH9Jylrxvq7uOLoq5zfuoHQnWc6g/2gv2x1qwSR7SEC',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //USERID 4
+        {
+          //   password: 'password',
+          username: 'Elan',
+          email: 'elan@user.io',
+          hashedPassword:
+            '$2a$10$NMkHOnZRabJH9Jylrxvq7uOLoq5zfuoHQnWc6g/2gv2x1qwSR7SEC',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //USERID 5
+        {
+          //   password: 'password',
+          username: 'Lana',
+          email: 'lana@user.io',
+          hashedPassword:
+            '$2a$10$NMkHOnZRabJH9Jylrxvq7uOLoq5zfuoHQnWc6g/2gv2x1qwSR7SEC',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //USERID 6
+        {
+          //   password: 'password',
+          username: 'Brian',
+          email: 'brian@user.io',
+          hashedPassword:
+            '$2a$10$NMkHOnZRabJH9Jylrxvq7uOLoq5zfuoHQnWc6g/2gv2x1qwSR7SEC',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
